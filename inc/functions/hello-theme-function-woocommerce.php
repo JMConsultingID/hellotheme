8c6d9db1ba8ec7b559af98c1c4fa51ed25abe0d9
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-05-31 18:40:55 +0800

    Update hello-theme-function-woocommerce.php

--- inc/functions/hello-theme-function-woocommerce.php
+++ inc/functions/hello-theme-function-woocommerce.php
@@ -17,7 +17,17 @@ function save_hello_theme_woo_settings() {
 }
 add_action( 'woocommerce_update_options_custom_redirects', 'save_hello_theme_woo_settings' );
 
+function hello_theme_get_pages_array() {
+    $pages = get_pages();
+    $pages_array = array();
+    foreach ( $pages as $page ) {
+        $pages_array[ $page->ID ] = $page->post_title;
+    }
+    return $pages_array;
+}
+
 function get_hello_theme_woo_settings() {
+    $pages = hello_theme_get_pages_array();
     $settings = array(
         'section_title' => array(
             'name'     => 'Hello Theme Redirects Settings',
@@ -27,21 +37,24 @@ function get_hello_theme_woo_settings() {
         ),
         'thank_you_page' => array(
             'name' => 'Thank You Page URL',
-            'type' => 'text',
+            'type' => 'select',
             'desc' => 'URL to redirect to after a successful order.',
-            'id'   => 'hello_theme_thank_you_page_url'
+            'id'   => 'hello_theme_thank_you_page_url',
+            'options' => $pages
         ),
         'failed_page' => array(
             'name' => 'Failed Order Page URL',
-            'type' => 'text',
+            'type' => 'select',
             'desc' => 'URL to redirect to after a failed order.',
-            'id'   => 'hello_theme_failed_page_url'
+            'id'   => 'hello_theme_failed_page_url',
+            'options' => $pages
         ),
         'on_hold_page' => array(
             'name' => 'On Hold Order Page URL',
-            'type' => 'text',
+            'type' => 'select',
             'desc' => 'URL to redirect to for on-hold orders.',
             'id'   => 'hello_theme_on_hold_page_url'
+            'options' => $pages
         ),
         'section_end' => array(
             'type' => 'sectionend',
