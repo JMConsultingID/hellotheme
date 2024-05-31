5362196457b7dfedaa1b37a266ccae0040b69649
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-05-31 15:38:41 +0800

    Initial commit

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * https://developers.elementor.com/docs/hello-elementor-theme/
+ *
+ * @package HelloTheme
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_THEME_VERSION', '2.0.1' );
+
+/**
+ * Load hello theme scripts & styles.
+ *
+ * @return void
+ */
+require_once get_stylesheet_directory() . '/inc/functions/hello-theme-functions.php';
+
+function hello_theme_scripts_styles() {
+    wp_enqueue_style(
+        'hello-theme-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        [],
+        HELLO_THEME_VERSION
+    );
+
+    // Enqueue the custom theme CSS
+    wp_enqueue_style(
+        'hello-theme-custom-style',
+        get_stylesheet_directory_uri() . '/assets/css/hello-theme.css',
+        [],
+        HELLO_THEME_VERSION
+    );
+
+    // Enqueue the custom theme JS
+    wp_enqueue_script(
+        'hello-theme-custom-script',
+        get_stylesheet_directory_uri() . '/assets/js/hello-theme.js',
+        [],
+        HELLO_THEME_VERSION,
+        true // Load script in the footer
+    );
+}
+add_action('wp_enqueue_scripts', 'hello_theme_scripts_styles', 20);
---
+++ inc/functions/hello-theme-functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * @package HelloTheme
+ */
+
+// Disable add to cart messages
+add_filter( 'wc_add_to_cart_message_html', '__return_false' );
+
+// Empty cart before adding a new item
+add_filter( 'woocommerce_add_cart_item_data', '_empty_cart' );
+function _empty_cart( $cart_item_data ) {
+    WC()->cart->empty_cart();
+    return $cart_item_data;
+}
+
+// Redirect to checkout page after adding an item to the cart
+add_filter('woocommerce_add_to_cart_redirect', 'hello_funding_add_to_cart_redirect');
+function hello_funding_add_to_cart_redirect() {
+    return wc_get_checkout_url();
+}
+
+// Disable non-base location price adjustments
+add_filter( 'woocommerce_adjust_non_base_location_prices', '__return_false' );
+
+// Disable order notes field
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
+
+// Change order button text
+add_filter( 'woocommerce_order_button_text', 'exf_wc_custom_order_button_text' );
+function exf_wc_custom_order_button_text() {
+    return __( 'PROCEED TO PAYMENT', 'woocommerce' );
+}
+
+?>
