2bfc3ea47f1e88ea407ff1216f1766d630b9279c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-01 15:02:54 +0800

    Update hello-theme.js

--- assets/js/hello-theme.js
+++ assets/js/hello-theme.js
@@ -1,6 +1,6 @@
 (function( $ ) {
 	'use strict';
-	$(document.body).on('country_to_state_changed', function() {
+	$(document).ready(function() {
 		$('.hello-theme-address-field').addClass('form-row-first'); 
         $('.hello-theme-country-field').addClass('form-row-first'); 
         $('.hello-theme-state-field').addClass('form-row-last');  
