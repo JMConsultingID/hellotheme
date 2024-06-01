78a645fe27c6246e51487ba911722ae16b22ad3a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-01 14:23:37 +0800

    Update hello-theme-functions.php

--- inc/functions/hello-theme-functions.php
+++ inc/functions/hello-theme-functions.php
@@ -36,30 +36,11 @@ function hello_theme_wc_custom_order_button_text() {
     return __( 'PROCEED TO PAYMENT', 'woocommerce' );
 }
 
-add_filter('woocommerce_checkout_fields', 'hello_theme_modify_woocommerce_billing_fields', 18, 1);
-function hello_theme_modify_woocommerce_billing_fields($fields)
-{   
+add_filter( 'woocommerce_checkout_fields' , 'hello_theme_modify_woocommerce_billing_fields' );
+function hello_theme_modify_woocommerce_billing_fields( $fields ) {
     // Remove Default Field
     unset($fields['billing']['billing_company']);
     unset($fields['billing']['billing_address_1']);    
-    unset($fields['billing']['billing_address_2']);
-
-    // Add Hello Theme Field
-    $fields['billing']['billing_address_1'] = array(
-        'type'        => 'text',
-        'label'       => __('Address', 'woocommerce'),
-        'required'    => true,
-        'class'       => array('address-field','hello-theme-checkout-field'),
-        'clear'       => true,
-        'priority'    => 40,
-    );
-    return $fields;
-}
-
-add_filter( 'woocommerce_checkout_fields' , 'ganti_alamat_penagihan' );
-function ganti_alamat_penagihan( $fields ) {
-    // Hapus field lama
-    unset($fields['billing']['billing_address_1']);
     unset($fields['billing']['billing_address_2']);
 
     // Tambahkan field baru
