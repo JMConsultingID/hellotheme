07c12bbc4c2f293b32fd5f7865d914a477873f65
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-07 09:09:12 +0800

    update core

--- inc/functions/hello-theme-functions.php
+++ inc/functions/hello-theme-functions.php
@@ -7,9 +7,12 @@
  *
  * @package HelloTheme
  */
-
+// Admin Settings
 require_once get_stylesheet_directory() . '/inc/functions/admin/hello-admin-function-settings-tab.php';
-require_once get_stylesheet_directory() . '/inc/functions/public/hello-public-function-redirect.php';
+
+// Public Settings
+require_once get_stylesheet_directory() . '/inc/functions/public/hello-public-function-affiliate-wp.php';
+require_once get_stylesheet_directory() . '/inc/functions/public/hello-public-function-redirect-page.php';
 require_once get_stylesheet_directory() . '/inc/functions/public/hello-public-function-woocommerce.php';
 
 ?>
