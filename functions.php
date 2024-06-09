4dc2f766123b7a1776f0ecd8b43ca7a0a740bcc9
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-06-09 23:28:11 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -23,22 +23,11 @@ define( 'HELLO_THEME_VERSION', '2.1.3' );
  */
 require_once get_stylesheet_directory() . '/inc/functions/hello-theme-functions.php';
 
-function hello_theme_scripts_styles() {
-    // Register styles        
-    wp_register_style( 'hello-theme-font-awesome-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
-    wp_register_style( 'hello-theme-swiper-bundle-css', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css');
-    wp_register_style( 'hello-theme-pricing-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-pricing-table.css', array('hello-theme-font-awesome-css', 'hello-theme-swiper-bundle-css'), '1.0.1', 'all' );
-
-
-    // Register scripts        
-    wp_register_script( 'hello-theme-swiper-bundle-js', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js', array('jquery'), null, true );
-    wp_register_script( 'hello-theme-popper-js', 'https://unpkg.com/@popperjs/core@2.11.8/dist/umd/popper.min.js', array(), null, true );
-    wp_register_script( 'hello-theme-tippy-js', 'https://unpkg.com/tippy.js@6.3.7/dist/tippy-bundle.umd.min.js', array(), null, true );
-
-    wp_register_script( 'hello-theme-pricing-js', get_stylesheet_directory_uri() . '/assets/js/hello-theme-pricing-table.js', array('jquery', 'hello-theme-swiper-bundle-js','hello-theme-popper-js', 'hello-theme-tippy-js'), '1.0.1', true );
-
+function hello_theme_scripts_styles() { 
     wp_enqueue_style('hello-theme-style', get_stylesheet_directory_uri() . '/style.css', [], HELLO_THEME_VERSION);
-    wp_enqueue_style('hello-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/hello-theme.css', [],      HELLO_THEME_VERSION);
+    wp_enqueue_style('hello-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/hello-theme.css', [],HELLO_THEME_VERSION);
     wp_enqueue_script('hello-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/hello-theme.js', [],HELLO_THEME_VERSION, true);
+    wp_enqueue_style( 'hello-theme-pricing-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-pricing-table.css', [],HELLO_THEME_VERSION );
+    wp_enqueue_script( 'hello-theme-pricing-js', get_stylesheet_directory_uri() . '/assets/js/hello-theme-pricing-table.js', [],HELLO_THEME_VERSION, true );
 }
 add_action('wp_enqueue_scripts', 'hello_theme_scripts_styles', 20);
