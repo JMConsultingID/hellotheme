eff9329881b9416271c621de060913596bc41ba5
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-07-28 23:55:52 +0800

    update core

--- inc/functions/hello-theme-functions.php
+++ inc/functions/hello-theme-functions.php
@@ -31,14 +31,15 @@ function hello_theme_pricing_table_live() {
     if ( get_option( 'hello_theme_enable_table_pricing' ) === '1' ) {
         // Enqueue styles        
         wp_enqueue_style( 'hello-theme-font-awesome-css', get_stylesheet_directory_uri() . '/assets/css/all.min.css');
-        wp_enqueue_style( 'hello-theme-swiper-bundle-css', get_stylesheet_directory_uri() . '/assets/css/\/swiper-bundle.min.css');
+        wp_enqueue_style( 'hello-theme-swiper-bundle-css', get_stylesheet_directory_uri() . '/assets/css/swiper-bundle.min.css');
         wp_enqueue_style( 'hello-theme-tippy-css', get_stylesheet_directory_uri() . '/assets/css/tippy.css');
+        wp_enqueue_style( 'hello-theme-tippy-light-css', get_stylesheet_directory_uri() . '/assets/css/tippy-light.css');
         wp_enqueue_style( 'hello-theme-plugins-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-pricing-table.css', array('hello-theme-font-awesome-css', 'hello-theme-swiper-bundle-css'), HELLO_THEME_VERSION, 'all' );
 
         // Enqueue scripts        
         wp_enqueue_script( 'hello-theme-swiper-bundle-js', get_stylesheet_directory_uri() . '/assets/js/swiper-bundle.min.js', array('jquery'), null, true );
         wp_enqueue_script( 'hello-theme-popper-js', get_stylesheet_directory_uri() . '/assets/js/popper.min.js', array(), null, true );
-        wp_enqueue_script( 'hello-theme-tippy-js', get_stylesheet_directory_uri() . '/assets/js//tippy-bundle.umd.min.js', array(), null, true );
+        wp_enqueue_script( 'hello-theme-tippy-js', get_stylesheet_directory_uri() . '/assets/js/tippy-bundle.umd.min.js', array(), null, true );
 
         if ( get_option( 'hello_theme_table_mode' ) === 'one_tab' ) {
         wp_enqueue_script( 'hello-theme-pricing-table-level-1-js', get_stylesheet_directory_uri() . '/assets/js/hello-theme-pricing-table.js', array('jquery', 'hello-theme-swiper-bundle-js','hello-theme-popper-js', 'hello-theme-tippy-js'), HELLO_THEME_VERSION, true );
