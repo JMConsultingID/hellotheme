1d6433a07a5ec156f453355c690e44b6d5142fdf
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-07-31 18:29:06 +0800

    Update order-received.php

--- woocommerce/checkout/order-received.php
+++ woocommerce/checkout/order-received.php
@@ -30,6 +30,7 @@ defined( 'ABSPATH' ) || exit;
 	 * @param string         $message The message.
 	 * @param WC_Order|false $order   The order created during checkout, or false if order data is not available.
 	 */
+	echo do_shortcode('[elementor-template id="29293"]');
 	$message = apply_filters(
 		'woocommerce_thankyou_order_received_text',
 		esc_html( __( 'Thank you. Your order has been received.', 'woocommerce' ) ),
