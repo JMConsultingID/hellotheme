f6d76e555b8ca87b88dc5ad672066854f5406563
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-02 00:10:10 +0800

    Update hello-public-function-affiliate-wp.php

--- inc/functions/public/hello-public-function-affiliate-wp.php
+++ inc/functions/public/hello-public-function-affiliate-wp.php
@@ -68,46 +68,20 @@ function hello_theme_affiliate_redirect() {
     // Get the full URL including query string.
     $full_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
+    // Match any URL that starts with /ref/ followed by any characters.
+    if (preg_match('|^/ref/.*|', $request_uri)) {
+        // Perform the redirection to the main site.
+        wp_redirect($redirect_referral_url, 301);
+        exit;
+    }
 
     // Check for the presence of 'ref' as a query parameter.
-    if (strpos($full_url, '?ref=') !== false || preg_match('|^/ref/[\w-]+/?|', $_SERVER['REQUEST_URI'])) {
-        // Construct the new URL to redirect to the homepage of the main site.
-        $new_url = $redirect_referral_url;
-
-        // Perform the redirection to the main site.
-        wp_redirect($new_url, 301);
-        exit;
-    }
-
-    // Match the /ref/{string}/ structure (with or without query parameters).
-    if (preg_match('|^/ref/([\w-]+)/?(\?.*)?$|', $request_uri, $matches)) {
-        // Extract the string from the matches.
-        $dynamic_string = $matches[1];
-
-        // Check for query string and extract if it exists.
-        $query_string = isset($matches[2]) ? $matches[2] : '';
-        
-        // Perform the redirection.
-        wp_redirect($redirect_referral_url, 301);
-        exit;
-    }
-    
-    // Use a regex pattern to match the /ref/{dynamic_number}/ structure.
-    if ( preg_match('|^/ref/([\d\w]+)/?$|', $request_uri, $matches)) {
-        // Extract the dynamic number from the matches.
-        $dynamic_value = $matches[1];
-                
-        // Perform the redirection.
-        wp_redirect($redirect_referral_url, 301);
-        exit;
-    }
-
-    // Check if the URL path is just a query string starting with ref.
-    if (preg_match('/^\?ref=\d+/', $request_uri)) {
+    if (strpos($full_url, '?ref=') !== false) {
         // Perform the redirection to the main site.
         wp_redirect($redirect_referral_url, 301);
         exit;
     }
 }
-add_action( 'template_redirect', 'hello_theme_affiliate_redirect',20 );
+add_action( 'template_redirect', 'hello_theme_affiliate_redirect', 20 );
+
 ?>
