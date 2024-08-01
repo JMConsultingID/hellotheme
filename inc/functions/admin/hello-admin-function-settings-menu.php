8970b23f2f348d40572e7cf129a5efacc93b7304
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-02 01:42:36 +0800

    Update hello-admin-function-settings-menu.php

--- inc/functions/admin/hello-admin-function-settings-menu.php
+++ inc/functions/admin/hello-admin-function-settings-menu.php
@@ -111,6 +111,7 @@ function hello_theme_register_settings() {
     register_setting( 'hello_woocommerce_settings_group', 'skip_cart_page' );
     register_setting( 'hello_woocommerce_settings_group', 'disable_shop_page' );
     register_setting( 'hello_woocommerce_settings_group', 'disable_product_page' );
+    register_setting( 'hello_woocommerce_settings_group', 'enable_ecommerce_tracking' );
     register_setting( 'hello_woocommerce_settings_group', 'hello_theme_thank_you_page_url' );
     register_setting( 'hello_woocommerce_settings_group', 'hello_theme_failed_page_url' );
     register_setting( 'hello_woocommerce_settings_group', 'hello_theme_on_hold_page_url' );
@@ -161,6 +162,15 @@ function hello_theme_register_settings() {
         'hello-woocommerce-settings',
         'hello_woocommerce_settings_section'
     );
+
+    add_settings_field(
+        'enable_ecommerce_tracking',
+        'Enable E-Commerce Tracking',
+        'hello_theme_enable_ecommerce_tracking_callback',
+        'hello-woocommerce-settings',
+        'hello_woocommerce_settings_section'
+    );
+
 
     add_settings_field(
         'hello_theme_thank_you_page_url',
@@ -341,6 +351,14 @@ function hello_theme_disable_product_page_callback() {
     <input type="checkbox" name="disable_product_page" value="1" <?php checked( 1, $options, true ); ?> />
     <?php
 }
+
+function hello_theme_enable_ecommerce_tracking_callback() {
+    $options = get_option( 'enable_ecommerce_tracking' );
+    ?>
+    <input type="checkbox" name="enable_ecommerce_tracking" value="1" <?php checked( 1, $options, true ); ?> />
+    <?php
+}
+
 
 function hello_theme_thank_you_page_url_callback() {
     $options = get_option( 'hello_theme_thank_you_page_url' );
