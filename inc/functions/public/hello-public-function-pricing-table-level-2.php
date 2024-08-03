d15384191dd9645308ce8580517879464a868a01
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-03 23:36:16 +0800

    Update hello-public-function-pricing-table-level-2.php

--- inc/functions/public/hello-public-function-pricing-table-level-2.php
+++ inc/functions/public/hello-public-function-pricing-table-level-2.php
@@ -38,9 +38,11 @@ function hello_pricing_table_level_2_shortcode() {
                     </div>
 
                     <?php foreach ($products as $productIndex => $product): ?>
-                        <div id="subtab-<?php echo $product->get_id(); ?>" class="hello-theme-sub-tab-content pricing__table hello-theme-product-id <?php echo $productIndex == 0 ? 'active' : ''; ?>" data-sub-tab-id="subtab-<?php echo $product->get_id(); ?>">
+                        <div id="subtab-<?php echo $product->get_id(); ?>" class="hello-theme-sub-tab-content <?php echo $productIndex == 0 ? 'active' : ''; ?>" data-sub-tab-id="subtab-<?php echo $product->get_id(); ?>">
 
                             <?php
+                                hello_theme_display_swiper_navigation_buttons('navBtnLeft', 'navBtnRight');
+                                $navigationButtons = ob_get_clean();
                                 $product_id = $product->get_id();
                                 $product_price = wc_price($product->get_price()); // Get product price with currency symbol
                                 $checkout_url = "/checkout/?add-to-cart={$product_id}"; // Generate checkout URL
@@ -54,77 +56,24 @@ function hello_pricing_table_level_2_shortcode() {
                                 $sale_price = get_post_meta($product_id, '_sale_price', true);
                             ?>
 
-                              <div class='pt__title'>
-                                <div class='pt__title__wrap'>
-                                  <div class='pt__row'><?php $product->get_name() ?></div>
-                                  <div class='pt__row'>Monthly Email Sends</div>
-                                  <div class='pt__row'>Users</div>
-                                  <div class='pt__row'>Audiences</div>
-                                  <div class='pt__row'>24/7 Email & Chat Support</div>
-                                  <div class='pt__row'>Pre-built Email Templates</div>
-                                  <div class='pt__row'>300+ Integrations</div>
-                                  <div class='pt__row'>Reporting & Analytics</div>
-                                  <div class='pt__row'>Forms & Landing Pages</div>
-                                  <div class='pt__row'>Creative Assistant</div>
+                            <div class='pricing__table hello-theme-product-id'>
+                                <div class='pt__title'>
+                                    <div class='pt__title__wrap'>
+                                      <div class='pt__row'>{$product->get_name()}</div>
+                                      <div class='pt__row'>Monthly Email Sends</div>
+                                      <div class='pt__row'>Users</div>
+                                      <div class='pt__row'>Audiences</div>
+                                      <div class='pt__row'>24/7 Email & Chat Support</div>
+                                      <div class='pt__row'>Pre-built Email Templates</div>
+                                      <div class='pt__row'>300+ Integrations</div>
+                                      <div class='pt__row'>Reporting & Analytics</div>
+                                      <div class='pt__row'>Forms & Landing Pages</div>
+                                      <div class='pt__row'>Creative Assistant</div>
+                                    </div>
                                 </div>
-                              </div>
-
-                            <div class="pt__option">
-                                <?php hello_theme_display_swiper_navigation_buttons('navBtnLeft', 'navBtnRight'); ?>
-                                <div class='pt__option__slider swiper' id='pricingTableSlider'>
-                                  <div class='swiper-wrapper'>
-                                    <div class='swiper-slide pt__option__item'>
-                                      <div class='pt__item'>
-                                        <div class='pt__item__wrap'>
-                                          <div class='pt__row'>Origin Funded Stater</div>
-                                          <div class='pt__row'>150,000</div>
-                                          <div class='pt__row'>Unlimited</div>
-                                          <div class='pt__row'>Unlimited</div>
-                                          <div class='pt__row'>Phone & Priority Support</div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div class='swiper-slide pt__option__item'>
-                                      <div class='pt__item'>
-                                        <div class='pt__item__wrap'>
-                                          <div class='pt__row'>Origin Funded Standard</div>
-                                          <div class='pt__row'>16,000</div>
-                                          <div class='pt__row'>5 Seats</div>
-                                          <div class='pt__row'>5 Audiences</div>
-                                          <div class='pt__row'>24/7 Email & Chat Support</div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div class='swiper-slide pt__option__item'>
-                                      <div class='pt__item'>
-                                        <div class='pt__item__wrap'>
-                                          <div class='pt__row'>Origin Funded Profesional</div>
-                                          <div class='pt__row'>5,000</div>
-                                          <div class='pt__row'>3 Seats</div>
-                                          <div class='pt__row'>3 Audiences</div>
-                                          <div class='pt__row'>24/7 Email & Chat Support</div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'>Limited</div>
-                                          <div class='pt__row'><i class='fa-solid fa-check'></i></div>
-                                          <div class='pt__row'>Limited</div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
+                                <div class='pt__option'>
                                 </div>
                             </div>
-
                         </div>
                     <?php endforeach; ?>
                 <?php else: ?>
