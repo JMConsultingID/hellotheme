88edfda4362939b95332a6feba519edf7707b48a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-03 23:22:54 +0800

    Update hello-public-function-pricing-table-level-2.php

--- inc/functions/public/hello-public-function-pricing-table-level-2.php
+++ inc/functions/public/hello-public-function-pricing-table-level-2.php
@@ -38,8 +38,39 @@ function hello_pricing_table_level_2_shortcode() {
                     </div>
 
                     <?php foreach ($products as $productIndex => $product): ?>
-                        <div id="subtab-<?php echo $product->get_id(); ?>" class="hello-theme-sub-tab-content <?php echo $productIndex == 0 ? 'active' : ''; ?>" data-sub-tab-id="subtab-<?php echo $product->get_id(); ?>">
-                            <?php echo render_pricing_table($product); ?>
+                        <div id="subtab-<?php echo $product->get_id(); ?>" class="hello-theme-sub-tab-content pricing__table hello-theme-product-id <?php echo $productIndex == 0 ? 'active' : ''; ?>" data-sub-tab-id="subtab-<?php echo $product->get_id(); ?>">
+
+                            <?php
+                                hello_theme_display_swiper_navigation_buttons('navBtnLeft', 'navBtnRight');
+                                $navigationButtons = ob_get_clean();
+                                $product_id = $product->get_id();
+                                $product_price = wc_price($product->get_price()); // Get product price with currency symbol
+                                $checkout_url = "/checkout/?add-to-cart={$product_id}"; // Generate checkout URL
+
+                                // ACF field group name
+                                $tooltip_post_id = 28386;
+                                $acf_group_field = 'fyfx_pricing_table';
+                                $acf_tooltip_group_field = 'fyfx_pricing_table_tooltips';
+
+                                $regular_price = get_post_meta($product_id, '_regular_price', true);
+                                $sale_price = get_post_meta($product_id, '_sale_price', true);
+                            ?>
+
+                              <div class='pt__title'>
+                                <div class='pt__title__wrap'>
+                                  <div class='pt__row'><?php $product->get_name() ?></div>
+                                  <div class='pt__row'>Monthly Email Sends</div>
+                                  <div class='pt__row'>Users</div>
+                                  <div class='pt__row'>Audiences</div>
+                                  <div class='pt__row'>24/7 Email & Chat Support</div>
+                                  <div class='pt__row'>Pre-built Email Templates</div>
+                                  <div class='pt__row'>300+ Integrations</div>
+                                  <div class='pt__row'>Reporting & Analytics</div>
+                                  <div class='pt__row'>Forms & Landing Pages</div>
+                                  <div class='pt__row'>Creative Assistant</div>
+                                </div>
+                              </div>
+
                         </div>
                     <?php endforeach; ?>
                 <?php else: ?>
