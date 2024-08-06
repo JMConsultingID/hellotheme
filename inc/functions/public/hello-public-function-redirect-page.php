20b59733947bee93884fbbfb3c101b7768078dd4
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-06 14:15:19 +0800

    Update hello-public-function-redirect-page.php

--- inc/functions/public/hello-public-function-redirect-page.php
+++ inc/functions/public/hello-public-function-redirect-page.php
@@ -11,7 +11,6 @@ function hello_theme_redirect_after_purchase( $order_id ) {
     if ( get_option( 'enable_thank_you_redirect' ) == '1' ) {
         $order = wc_get_order( $order_id );
         $status = $order->get_status();
-        $order_key = $order->get_order_key();
 
         $thank_you_page_id = get_option( 'hello_theme_thank_you_page_url' );
         $failed_page_id = get_option( 'hello_theme_failed_page_url' );
@@ -48,7 +47,15 @@ add_action( 'woocommerce_thankyou', 'hello_theme_redirect_after_purchase' );
 add_action('wp_footer', 'hello_theme_add_ga_gtm_script_to_thank_you_page', 10);
 
 function hello_theme_add_ga_gtm_script_to_thank_you_page() {
-    if (is_page(2662) && isset($_GET['order_id']) && isset($_GET['order_key'])) {
+    $enable_ecommerce_tracking = get_option( 'enable_ecommerce_tracking' );
+    $thank_you_page_id = get_option( 'hello_theme_thank_you_page_url' );
+
+    // If the $enable_ecommerce_tracking is not '1', return early
+    if ($enable_ecommerce_tracking !== '1') {
+        return;
+    }
+
+    if (is_page($thank_you_page_id) && isset($_GET['order_id']) && isset($_GET['order_key'])) {
         $order_id = sanitize_text_field($_GET['order_id']);
         $order = wc_get_order($order_id);
 
@@ -74,22 +81,12 @@ function hello_theme_add_ga_gtm_script_to_thank_you_page() {
             if ($status === 'completed') {
                 ?>
                 <script>
-                    // GA4 Event
+                    // GTM Event
                     gtag('event', 'purchase', {
                         "transaction_id": "<?php echo $transaction_id; ?>",
                         "value": <?php echo $transaction_total; ?>,
                         "currency": "<?php echo $currency; ?>",
                         "items": <?php echo $products_json; ?>
-                    });
-
-                    // GTM Event
-                    window.dataLayer = window.dataLayer || [];
-                    window.dataLayer.push({
-                        'event': 'purchase',
-                        'transactionId': '<?php echo $transaction_id; ?>',
-                        'transactionTotal': '<?php echo $transaction_total; ?>',
-                        'transactionCurrency': '<?php echo $currency; ?>',
-                        'transactionProducts': <?php echo $products_json; ?>
                     });
                 </script>
                 <?php
@@ -97,45 +94,62 @@ function hello_theme_add_ga_gtm_script_to_thank_you_page() {
                 ?>
                 <script>
                     // GA4 Event
-                    gtag('event', 'page_view', {
+                    gtag('event', 'not_completed', {
                         "items": <?php echo $products_json; ?>
                     });
 
                     // GTM Event
                     window.dataLayer = window.dataLayer || [];
                     window.dataLayer.push({
-                        'event': 'page_view',
+                        'event': 'not_completed',
                         'transactionProducts': <?php echo $products_json; ?>
                     });
                 </script>
                 <?php
             }
         }
+    } else {
+        ?>
+        <script>
+            // GA4 Event
+            gtag('event', 'page_view', {
+                "page_id": <?php echo $thank_you_page_id; ?>
+            });
+            // GTM Event
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                'event': 'page_view',
+                'page_id': <?php echo $thank_you_page_id; ?>
+            });
+        </script>
+        <?php
     }
 }
 
-
 function hello_theme_redirect_cart_to_home() {
-    if ( get_option( 'enable_thank_you_redirect' ) == '1' ) {
-        if ( get_option( 'skip_cart_page' ) == '1' && ( is_page( 'cart' ) || ( isset( $_GET['cancel_order'] ) && $_GET['cancel_order'] === 'true' ) ) ) {
+    // Check if WooCommerce is installed and active
+    if ( class_exists( 'WooCommerce' ) ) {
+        if ( get_option( 'enable_thank_you_redirect' ) == '1' ) {
+            if ( get_option( 'skip_cart_page' ) == '1' && ( is_page( 'cart' ) || ( isset( $_GET['cancel_order'] ) && $_GET['cancel_order'] === 'true' ) ) ) {
+                $home_page_url = home_url();
+                wp_safe_redirect( $home_page_url );
+                exit;
+            }
+        }
+
+        // Check if shop page is disabled
+        if ( is_shop() && get_option( 'disable_shop_page' ) == '1' ) {
             $home_page_url = home_url();
             wp_safe_redirect( $home_page_url );
             exit;
         }
-    }
 
-    // Check if shop page is disabled
-    if ( is_shop() && get_option( 'disable_shop_page' ) == '1' ) {
-        $home_page_url = home_url();
-        wp_safe_redirect( $home_page_url );
-        exit;
-    }
-
-    // Check if product page is disabled
-    if ( is_product() && get_option( 'disable_product_page' ) == '1' ) {
-        $home_page_url = home_url();
+        // Check if product page is disabled
+        if ( is_product() && get_option( 'disable_product_page' ) == '1' ) {
+            $home_page_url = home_url();
             wp_safe_redirect( $home_page_url );
-        exit;
+            exit;
+        }
     }
 }
 add_action( 'template_redirect', 'hello_theme_redirect_cart_to_home' );
