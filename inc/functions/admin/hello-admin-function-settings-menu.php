33138713b4f83f65459ed9e6c356f936ad51a5a4
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-15 12:04:07 +0800

    Update hello-admin-function-settings-menu.php

--- inc/functions/admin/hello-admin-function-settings-menu.php
+++ inc/functions/admin/hello-admin-function-settings-menu.php
@@ -355,10 +355,12 @@ function hello_affiliatewp_settings_section_callback() {
 }
 
 function hello_table_pricing_settings_section_callback() {
+    $plugin_url = plugin_dir_url(__FILE__) . 'inc/functions/import/acf-export-ypf-default-2024.json';
     echo '<p>Configure & Generate your Pricing Table settings below.</p>';
     echo '<p><strong>Download ACF Template:</strong> If you want to use a pre-built ACF template for your pricing table, you can download the JSON file from the link below and import it into your ACF settings.</p>';
-    echo '<a href="' . plugin_dir_url( __FILE__ ) . 'inc/functions/import/acf-export-ypf-default-2024.json" download>Download ACF Template JSON File</a>';
+    echo '<a href="' . esc_url($plugin_url) . '" download>Download ACF Template JSON File</a>';
 }
+
 
 function hello_theme_checkout_mode_callback() {
     $options = get_option( 'hello_theme_checkout_mode' );
