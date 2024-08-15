e190fa0b1d220690d6ff59e1e147077cc65b948d
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-15 17:28:30 +0800

    Update hello-public-function-pricing-table-level-2.php

--- inc/functions/public/hello-public-function-pricing-table-level-2.php
+++ inc/functions/public/hello-public-function-pricing-table-level-2.php
@@ -177,10 +177,10 @@ function hello_pricing_table_level_2_shortcode($atts) {
                                     
                                   </div>
 
-                            </div>                            
-                        </div>
-                        <div class="hello-theme-checkout-button">
-                            <a href="<?php echo $checkout_url; ?>">Purchase Now (<?php echo $product_price;?>)</a>
+                            </div>
+                            <div class="hello-theme-checkout-button">
+                                <a href="<?php echo $checkout_url; ?>">Purchase Now (<?php echo $product_price;?>)</a>
+                            </div>
                         </div>
 
                     <?php endforeach; ?>
