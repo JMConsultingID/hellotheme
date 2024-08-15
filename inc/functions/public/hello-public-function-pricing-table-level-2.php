5acdf239125decfca4aa3a2b76d2d86f1e91dd5c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-15 14:09:56 +0800

    Update hello-public-function-pricing-table-level-2.php

--- inc/functions/public/hello-public-function-pricing-table-level-2.php
+++ inc/functions/public/hello-public-function-pricing-table-level-2.php
@@ -146,7 +146,7 @@ function hello_pricing_table_level_2_shortcode($atts) {
                                                         if (!is_null($level_fields) && is_array($level_fields)) :
                                                             foreach ($level_fields as $field_key => $field_value) : ?>
                                                             <div class="pt__row <?php echo esc_html($field_key); ?>">
-                                                                <?php echo !empty($field_value) ? esc_html($field_value) : 'N/A'; ?>
+                                                                <?php echo !empty($field_value) ? $field_value : 'N/A'; ?>
                                                             </div>
                                                             <?php endforeach; ?>
                                                         <?php endif; ?>
