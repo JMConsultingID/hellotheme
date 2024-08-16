bc57f5fa6ad985e8c73a6002a7d721dc9b8510cc
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-16 15:27:48 +0800

    Update hello-theme-functions.php

--- inc/functions/hello-theme-functions.php
+++ inc/functions/hello-theme-functions.php
@@ -41,7 +41,7 @@ function hello_theme_pricing_table_live() {
         wp_enqueue_style( 'hello-theme-swiper-bundle-css', get_stylesheet_directory_uri() . '/assets/css/swiper-bundle.min.css');
         wp_enqueue_style( 'hello-theme-tippy-css', get_stylesheet_directory_uri() . '/assets/css/tippy.css');
         wp_enqueue_style( 'hello-theme-tippy-light-css', get_stylesheet_directory_uri() . '/assets/css/tippy-light.css');
-        wp_enqueue_style( 'hello-theme-plugins-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-pricing-table.css', array('hello-theme-font-awesome-css', 'hello-theme-swiper-bundle-css, hello-theme-tippy-css, hello-theme-tippy-light-css'), HELLO_THEME_VERSION, 'all' );
+        wp_enqueue_style( 'hello-theme-plugins-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-pricing-table.css', array('hello-theme-swiper-bundle-css, hello-theme-tippy-css, hello-theme-tippy-light-css'), HELLO_THEME_VERSION, 'all' );
 
         // Enqueue scripts        
         wp_enqueue_script( 'hello-theme-swiper-bundle-js', get_stylesheet_directory_uri() . '/assets/js/swiper-bundle.min.js', array('jquery'), null, true );
