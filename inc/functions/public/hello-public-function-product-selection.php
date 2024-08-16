f6bb8d92e6407bfa6e5e8024d77bc87b83db5ff7
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-16 12:41:32 +0800

    Update hello-public-function-product-selection.php

--- inc/functions/public/hello-public-function-product-selection.php
+++ inc/functions/public/hello-public-function-product-selection.php
@@ -12,7 +12,7 @@ function hello_theme_challenge_selection_shortcode($atts) {
     $enabled_pricing_table = get_option('hello_theme_enable_table_pricing');
     $atts = shortcode_atts(
         array(
-            'mode' => 'mode-1',
+            'tab_mode' => 'mode-1',
             'category' => '',
         ),
         $atts,
@@ -22,11 +22,55 @@ function hello_theme_challenge_selection_shortcode($atts) {
     if ($enabled_pricing_table !== '1') {
         return;
     }
+
+    $tab_mode = $atts['tab_mode'];
+
+    // Parse the categories from the shortcode attribute
+    $category_slugs = explode(',', $atts['category']);
+    $categories = get_terms(array(
+        'taxonomy' => 'product_cat',
+        'slug' => $category_slugs,
+        'hide_empty' => false,
+    ));
+
     ob_start();
     ?>
     <div class="hello-theme-product selection product-selection-container product-id">
         <!-- Add your HTML here as per the uploaded design -->
-        <p>No products found in this category.</p>
+        <div class="hello-theme-container hello-theme-table-pricing hello-theme-with-tab hello-theme-table-<?php echo $tab_mode; ?>">
+            <div class="hello-theme-tab-buttons">
+                <?php foreach ($categories as $index => $category): ?>
+                    <div class="hello-theme-tab-button <?php echo $index == 0 ? 'active' : ''; ?>" data-tab-id="tab-<?php echo $category->term_id; ?>">
+                        <?php echo $category->name; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+            <?php foreach ($categories as $index => $category): ?>
+            <div id="tab-<?php echo $category->term_id; ?>" class="hello-theme-tab-content category-<?php echo $category->slug; ?> <?php echo $index == 0 ? 'active' : ''; ?>" data-tab-id="tab-<?php echo $category->term_id; ?>">
+                <?php
+                $products = wc_get_products(array(
+                    'category' => array($category->slug),
+                    'status' => 'publish',
+                    'orderby' => 'menu_order',
+                    'order' => 'ASC'
+                ));
+                if ($products): ?>
+                    <div class="hello-theme-sub-tab-buttons">
+                        <?php foreach ($products as $productIndex => $product): ?>
+                            <div class="hello-theme-sub-tab-button <?php echo $productIndex == 0 ? 'active' : ''; ?>" data-sub-tab-id="subtab-<?php echo $product->get_id(); ?>">
+                                <?php echo $product->get_name(); ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php else : ?>
+                    <div class="hello-theme-sub-tab-buttons">
+                        <p>No products found in this category.</p>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php endforeach; ?>
+        </div>
     </div>
     <?php
     return ob_get_clean();
