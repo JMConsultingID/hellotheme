001fe64d558d000a592e1a5a9708589b08b8a0b6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-26 23:58:46 +0800

    Update hello-theme-functions.php

--- inc/functions/hello-theme-functions.php
+++ inc/functions/hello-theme-functions.php
@@ -66,7 +66,7 @@ function hello_theme_enqueue_product_selection() {
     $enabled_product_selection = get_option('enable_product_selection_pages');
     if ($enabled_product_selection === '1') {
         wp_enqueue_style( 'hello-theme-products-selection-challenge-css', get_stylesheet_directory_uri() . '/assets/css/hello-theme-product-selection.css', array(), HELLO_THEME_VERSION, 'all' );
-        wp_enqueue_script('hello-theme-products-selection-challenge-js', get_stylesheet_directory_uri() . '/assets/js/hello-theme-product-selection.js', array('jquery'), null, true);
+        wp_enqueue_script('hello-theme-products-selection-challenge-js', get_stylesheet_directory_uri() . '/assets/js/hello-theme-product-selection.js', array('jquery'), HELLO_THEME_VERSION, true);
         wp_localize_script('hello-theme-products-selection-challenge-js', 'ajax_object', array('ajaxurl' => admin_url('admin-ajax.php')));
     }
 }
