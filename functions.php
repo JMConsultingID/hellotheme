adf1f1b20c538f4e6ef43a85b9e1e94346be15d9
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-26 23:44:40 +0800

    update core

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-define('HELLO_THEME_VERSION', '2.1.9');
+define('HELLO_THEME_VERSION', '2.1.10');
 
 /**
  * Load hello theme scripts & styles.
