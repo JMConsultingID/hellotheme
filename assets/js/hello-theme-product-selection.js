c5efa61a81b9471f7c150fe66c144eabf70fa40a
eliaser-99co <elp@finpr.com>
2024-08-26 13:10:22 +0800

    Update hello-theme-product-selection - input range price

--- assets/js/hello-theme-product-selection.js
+++ assets/js/hello-theme-product-selection.js
@@ -1,158 +1,217 @@
-(function( $ ) {
-    'use strict';
+(function ($) {
+  "use strict";
 
-    $(document).ready(function() {
-        const categorySelection = document.querySelector('#category-selection');
-        const challengeButtons = document.querySelectorAll('.challenge-option');
-        const accountTypeSelection = document.querySelector('#account-type-selection');
-        const addonsSelection = document.querySelector('#addons-selection ul');
-        const checkoutButton = document.querySelector('#checkout-button');
-        const productImage = document.querySelector('#product-image');
-        const productTitle = document.querySelector('#product-title');
-        const productDescription = document.querySelector('#product-description');
-        const productPrice = document.querySelector('#product-price');
+  $(document).ready(function () {
+    const categorySelection = document.querySelector("#category-selection");
+    const challengeButtons = document.querySelectorAll(".challenge-option");
+    const accountTypeSelection = document.querySelector(
+      "#account-type-selection"
+    );
+    const addonsSelection = document.querySelector("#addons-selection ul");
+    const checkoutButton = document.querySelector("#checkout-button");
+    const productImage = document.querySelector("#product-image");
+    const productTitle = document.querySelector("#product-title");
+    const productDescription = document.querySelector("#product-description");
+    const productPrice = document.querySelector("#product-price");
 
-        // Preselect based on data attributes from the root element
-        const form = document.querySelector('#challenge-selection-form');
-        let selectedCategory = form.dataset.category;
-        let selectedChallenge = form.dataset.challenge;
-        let selectedAccountType = form.dataset.accountType;
-        let selectedAddons = [];
+    // Preselect based on data attributes from the root element
+    const form = document.querySelector("#challenge-selection-form");
+    let selectedCategory = form.dataset.category;
+    let selectedChallenge = form.dataset.challenge;
+    let selectedAccountType = form.dataset.accountType;
+    let selectedAddons = [];
 
-        // Apply preselect based on URL parameters or defaults
-        function applyPreselect() {
-            // Set category selection
-            document.querySelector(`input[name="category"][value="${selectedCategory}"]`).checked = true;
+    // Apply preselect based on URL parameters or defaults
+    function applyPreselect() {
+      // Set category selection
+      document.querySelector(
+        `input[name="category"][value="${selectedCategory}"]`
+      ).checked = true;
 
-            // Set challenge button
-            challengeButtons.forEach(btn => {
-                btn.classList.remove('selected');
-                if (btn.dataset.value === selectedChallenge) {
-                    btn.classList.add('selected');
-                }
-            });
-
-            // Set account type selection
-            document.querySelectorAll('input[name="account_type"]').forEach(function(radio) {
-                radio.checked = radio.value === selectedAccountType;
-            });
-
-            // Set addons selection
-            updateAddonsSelection();
+      // Set challenge button
+      challengeButtons.forEach((btn) => {
+        btn.classList.remove("selected");
+        if (btn.dataset.value === selectedChallenge) {
+          btn.classList.add("selected");
         }
+      });
 
-        // Update addons selection based on selected category
-        function updateAddonsSelection() {
-            addonsSelection.innerHTML = ''; // Clear previous addons
+      // Set account type selection
+      document
+        .querySelectorAll('input[name="account_type"]')
+        .forEach(function (radio) {
+          radio.checked = radio.value === selectedAccountType;
+        });
 
-            if (selectedCategory === 'base-camp') {
-                addonsSelection.innerHTML = `
+      // Set addons selection
+      updateAddonsSelection();
+    }
+
+    // Update addons selection based on selected category
+    function updateAddonsSelection() {
+      addonsSelection.innerHTML = ""; // Clear previous addons
+
+      if (selectedCategory === "base-camp") {
+        addonsSelection.innerHTML = `
                     <li><input type="checkbox" class="input-checkbox" name="addons" id="active-days" value="active-days"/><label for="active-days" class="">Active Days: 21 days + 15% fee</label></li>
                     <li><input type="checkbox" class="input-checkbox" name="addons" id="profitsplit" value="profitsplit"/><label for="profitsplit" class="">Profit Split: 50%/70%/80% + 20% fee</label></li>
                 `;
-            } else if (selectedCategory === 'the-peak') {
-                addonsSelection.innerHTML = `
+      } else if (selectedCategory === "the-peak") {
+        addonsSelection.innerHTML = `
                     <li><input type="checkbox" class="input-checkbox" name="addons" id="peak_active_days" value="peak_active_days"/><label for="peak_active_days" class="">Active Days: bi-weekly: + 20% fee</label></li>
                     <li><input type="checkbox" class="input-checkbox" name="addons" id="tradingdays" value="tradingdays"/><label for="tradingdays" class="">Trading Days: no minimum trading days + 15% fee</label></li>
                 `;
-            }
+      }
 
-            // Reset addons selection
-            selectedAddons = [];
-            document.querySelectorAll('input[name="addons"]').forEach(function(checkbox) {
-                checkbox.checked = form.dataset[checkbox.value] === 'yes'; // Check based on URL parameter or default
-                checkbox.addEventListener('change', function() {
-                    selectedAddons = Array.from(document.querySelectorAll('input[name="addons"]:checked')).map(el => el.value);
-                    updateCheckoutButton();
-                });
-            });
-
-            updateCheckoutButton(); // Update checkout button after setting addons
-        }
-
-        // Event listener for category selection change
-        categorySelection.addEventListener('change', function(e) {
-            selectedCategory = e.target.value;
-
-            // Reset preselect to default when category changes
-            if (selectedCategory === 'base-camp' || selectedCategory === 'the-peak') {
-                selectedChallenge = '10k';
-                selectedAccountType = 'standard';
-
-                // Set default selections
-                applyPreselect();
-            }
-
-            // Reset addons
-            updateAddonsSelection();
-            
-            // Update checkout button state
+      // Reset addons selection
+      selectedAddons = [];
+      document
+        .querySelectorAll('input[name="addons"]')
+        .forEach(function (checkbox) {
+          checkbox.checked = form.dataset[checkbox.value] === "yes"; // Check based on URL parameter or default
+          checkbox.addEventListener("change", function () {
+            selectedAddons = Array.from(
+              document.querySelectorAll('input[name="addons"]:checked')
+            ).map((el) => el.value);
             updateCheckoutButton();
+          });
         });
 
-        // Event listeners for challenge buttons
-        challengeButtons.forEach(button => {
-            button.addEventListener('click', function() {
-                selectedChallenge = button.getAttribute('data-value');
-                challengeButtons.forEach(btn => btn.classList.remove('selected'));
-                button.classList.add('selected');
-                updateCheckoutButton();
-            });
-        });
+      updateCheckoutButton(); // Update checkout button after setting addons
+    }
 
-        // Event listener for account type selection change
-        accountTypeSelection.addEventListener('change', function(e) {
-            selectedAccountType = e.target.value;
-            updateCheckoutButton();
-        });
+    // Event listener for category selection change
+    categorySelection.addEventListener("change", function (e) {
+      selectedCategory = e.target.value;
 
-        // Update checkout button and display product details based on selections
-        function updateCheckoutButton() {
-            // Check if all selections are made
-            if (!selectedCategory || !selectedChallenge || !selectedAccountType) {
-                checkoutButton.href = '#';
-                checkoutButton.setAttribute('disabled', 'disabled');
-                productImage.innerHTML = '';
-                productTitle.innerHTML = '';
-                productDescription.innerHTML = '';
-                productPrice.innerHTML = '';
-                return;
-            }
+      // Reset preselect to default when category changes
+      if (selectedCategory === "base-camp" || selectedCategory === "the-peak") {
+        selectedChallenge = "10k";
+        selectedAccountType = "standard";
 
-            const data = {
-                action: 'get_custom_product_id',
-                category: selectedCategory,
-                account_type: selectedAccountType,
-                challenge: selectedChallenge,
-                active_days: selectedAddons.includes('active-days') ? 'yes' : 'no',
-                profitsplit: selectedAddons.includes('profitsplit') ? 'yes' : 'no',
-                peak_active_days: selectedAddons.includes('peak_active_days') ? 'yes' : 'no',
-                tradingdays: selectedAddons.includes('tradingdays') ? 'yes' : 'no'
-            };
-
-            $.post(ajax_object.ajaxurl, data, function(response) {
-                if (response.success) {
-                    checkoutButton.href = '/checkout/?add-to-cart=' + response.data.product_id;
-                    checkoutButton.removeAttribute('disabled');
-
-                    // Update product image, title, description, and price
-                    productImage.innerHTML = `<img src="${response.data.product_image}" alt="${response.data.product_title}" class="product-img" />`;
-                    productTitle.innerHTML = `<h5 class="sub-title">${response.data.product_title}</h5>`;
-                    productDescription.innerHTML = `${response.data.product_description}`;
-                    productPrice.innerHTML = `<span class="num-total">${response.data.product_price}</span>`;
-                } else {
-                    checkoutButton.href = '#';
-                    checkoutButton.setAttribute('disabled', 'disabled');
-                    productImage.innerHTML = '';
-                    productTitle.innerHTML = '';
-                    productDescription.innerHTML = '';
-                    productPrice.innerHTML = '';
-                }
-            });
-        }
-
-        // Initialize addons and checkout button
+        // Set default selections
         applyPreselect();
+      }
+
+      // Reset addons
+      updateAddonsSelection();
+
+      // Update checkout button state
+      updateCheckoutButton();
     });
 
-})( jQuery );
+    // Event listeners for challenge buttons
+    challengeButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        selectedChallenge = button.getAttribute("data-value");
+        challengeButtons.forEach((btn) => btn.classList.remove("selected"));
+        button.classList.add("selected");
+        updateCheckoutButton();
+      });
+    });
+
+    // Event listener for account type selection change
+    accountTypeSelection.addEventListener("change", function (e) {
+      selectedAccountType = e.target.value;
+      updateCheckoutButton();
+    });
+
+    // Update checkout button and display product details based on selections
+    function updateCheckoutButton() {
+      // Check if all selections are made
+      if (!selectedCategory || !selectedChallenge || !selectedAccountType) {
+        checkoutButton.href = "#";
+        checkoutButton.setAttribute("disabled", "disabled");
+        productImage.innerHTML = "";
+        productTitle.innerHTML = "";
+        productDescription.innerHTML = "";
+        productPrice.innerHTML = "";
+        return;
+      }
+
+      const data = {
+        action: "get_custom_product_id",
+        category: selectedCategory,
+        account_type: selectedAccountType,
+        challenge: selectedChallenge,
+        active_days: selectedAddons.includes("active-days") ? "yes" : "no",
+        profitsplit: selectedAddons.includes("profitsplit") ? "yes" : "no",
+        peak_active_days: selectedAddons.includes("peak_active_days")
+          ? "yes"
+          : "no",
+        tradingdays: selectedAddons.includes("tradingdays") ? "yes" : "no",
+      };
+
+      $.post(ajax_object.ajaxurl, data, function (response) {
+        if (response.success) {
+          checkoutButton.href =
+            "/checkout/?add-to-cart=" + response.data.product_id;
+          checkoutButton.removeAttribute("disabled");
+
+          // Update product image, title, description, and price
+          productImage.innerHTML = `<img src="${response.data.product_image}" alt="${response.data.product_title}" class="product-img" />`;
+          productTitle.innerHTML = `<h5 class="sub-title">${response.data.product_title}</h5>`;
+          productDescription.innerHTML = `${response.data.product_description}`;
+          productPrice.innerHTML = `<span class="num-total">${response.data.product_price}</span>`;
+        } else {
+          checkoutButton.href = "#";
+          checkoutButton.setAttribute("disabled", "disabled");
+          productImage.innerHTML = "";
+          productTitle.innerHTML = "";
+          productDescription.innerHTML = "";
+          productPrice.innerHTML = "";
+        }
+      });
+    }
+
+    /* Handling Progress Bar */
+    function setupProgressBar(rangeSelector, tabsSelector) {
+      const range = document.querySelector(rangeSelector);
+      const tabs = document.querySelectorAll(tabsSelector);
+
+      // Update max value of range input based on the number of tabs
+      range.setAttribute("max", tabs.length);
+
+      // Initialize progress bar
+      range.style.backgroundSize =
+        ((range.value - range.min) / (range.max - range.min)) * 100 + "% 100%";
+
+      // Add event listener for range input
+      range.addEventListener("input", () => {
+        const currentVal = parseInt(range.value);
+        const min = parseInt(range.min);
+        const max = parseInt(range.max);
+
+        range.style.backgroundSize =
+          ((currentVal - min) / (max - min)) * 100 + "% 100%";
+
+        tabs.forEach((tab, index) => {
+          const tabIndex = index + 1;
+          const isSelected = tabIndex === currentVal;
+
+          tab.setAttribute("aria-selected", isSelected);
+          tab.setAttribute("tabindex", isSelected ? "0" : "-1");
+        });
+      });
+
+      // Add event listener for clicking tabs
+      tabs.forEach((tab) => {
+        tab.addEventListener("click", (e) => {
+          const tabIndex = parseInt(tab.getAttribute("data-tab-index"));
+          range.value = tabIndex;
+          setTimeout(() => {
+            range.dispatchEvent(new Event("input"));
+          }, 100);
+          e.preventDefault();
+        });
+      });
+    }
+    setupProgressBar(
+      "#sac-progress-range",
+      "#challenge-selection-bar .challenge-option"
+    );
+
+    // Initialize addons and checkout button
+    applyPreselect();
+  });
+})(jQuery);
