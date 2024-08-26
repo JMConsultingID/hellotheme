c2a98a76103e319fb7db780890f4a5df51398ad6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-08-26 22:59:15 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -26,8 +26,8 @@ require_once get_stylesheet_directory() . '/inc/functions/hello-theme-functions.
 function hello_theme_scripts_styles()
 {
     wp_enqueue_style('hello-theme-style', get_stylesheet_directory_uri() . '/style.css', [], HELLO_THEME_VERSION);
-    wp_enqueue_style('hello-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/hello-theme.css', [], HELLO_THEME_VERSION);
-    wp_enqueue_script('hello-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/hello-theme.js', [], HELLO_THEME_VERSION, true);
+    //wp_enqueue_style('hello-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/hello-theme.css', [], HELLO_THEME_VERSION);
+    //wp_enqueue_script('hello-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/hello-theme.js', [], HELLO_THEME_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'hello_theme_scripts_styles', 20);
 
