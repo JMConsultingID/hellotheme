9b21390be81dd68df9d072780bc6fbeabc8e0330
eliaser-99co <elp@finpr.com>
2024-08-28 11:16:08 +0800

    update theme version

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-define('HELLO_THEME_VERSION', '2.1.38');
+define('HELLO_THEME_VERSION', '2.1.39');
 
 /**
  * Load hello theme scripts & styles.
