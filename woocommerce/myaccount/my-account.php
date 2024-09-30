ea88ebb30c67573e06e5aec08085b1cb02a019db
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-09-30 14:26:46 +0800

    update core

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -32,7 +32,7 @@ defined( 'ABSPATH' ) || exit;
         <div class="woocommerce-dashboard-navigation dashboard-column col-lg-2 col-md-3 d-none d-md-block dashboard-sidebar fixed-sidebar">
             <?php 
                 echo '<div class="dashboard-logo">';
-		        echo '<img src="https://yourrobotrader.com/wp-content/uploads/2022/08/new-logo-12-jan-14-1024x158.webp" alt="Your Logo" class="img-fluid mb-3">';
+		        echo '<a href="/my-account/"><img src="https://yourrobotrader.com/wp-content/uploads/2022/08/new-logo-12-jan-14-1024x158.webp" alt="Your Robo Trader" class="img-fluid mb-3" width="85%"></a>';
 		        echo '</div>';
                 https://yourrobotrader.com/wp-content/uploads/2022/08/new-logo-12-jan-14-1024x158.webp
                 do_action( 'woocommerce_account_navigation' ); 
