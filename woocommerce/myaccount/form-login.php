370b901a426218c314852b6692c9a1a30390e66e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-02 15:04:49 +0800

    Update form-login.php

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -58,7 +58,7 @@ defined( 'ABSPATH' ) || exit;
 
         <!-- Forgot Password -->
         <div class="d-flex justify-content-end mb-4">
-            <a href="#"><?php esc_html_e( 'Forgot your password?', 'woocommerce' ); ?></a>
+            <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Forgot your password?', 'woocommerce' ); ?></a>
         </div>
 
         <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
