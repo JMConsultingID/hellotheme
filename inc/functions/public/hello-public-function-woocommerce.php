b3337d673bbfb265e11ad9b25a37dcc305db8766
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-04 10:30:00 +0800

    Update hello-public-function-woocommerce.php

--- inc/functions/public/hello-public-function-woocommerce.php
+++ inc/functions/public/hello-public-function-woocommerce.php
@@ -184,7 +184,7 @@ function hello_theme_display_order_status_shortcode( $atts ) {
     $order_status = wc_get_order_status_name( $order->get_status() );
 
     // Display the order status
-    return esc_html( $order_status );
+    return '<p>Order Status : ' . esc_html( $order_status ) . '</p>';
 }
 
 // Register the shortcode
