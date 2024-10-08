14b2bd9e51292718d1eafa290e99e78bc2c59625
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-08 11:22:08 +0800

    Update order-received.php

--- woocommerce/checkout/order-received.php
+++ woocommerce/checkout/order-received.php
@@ -43,14 +43,14 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php if ( $order->has_status( 'failed' ) ) : ?>
 
-		<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed text-center"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+		<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed text-center" style="margin-top: 20px;"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
 
-		<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions text-center">
+		<!-- <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions text-center">
 			<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
 			<?php if ( is_user_logged_in() ) : ?>
 				<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
 			<?php endif; ?>
-		</p>
+		</p> -->
 
 	<?php endif; ?>
 </p>
