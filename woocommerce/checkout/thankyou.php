0f8e5e5ff7f2fda409950b7e1b8766ee95974e64
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-08 11:09:53 +0800

    Update thankyou.php

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -28,19 +28,6 @@ defined( 'ABSPATH' ) || exit;
 		do_action( 'woocommerce_before_thankyou', $order->get_id() );
 		?>
 
-<!-- 		<?php if ( $order->has_status( 'failed' ) ) : ?>
-
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
-
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
-				<?php endif; ?>
-			</p>
-
-		<?php else : ?> -->
-
 			<?php wc_get_template( 'checkout/order-received.php', array( 'order' => $order ) ); ?>
 
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
@@ -75,8 +62,6 @@ defined( 'ABSPATH' ) || exit;
 				<?php endif; ?>
 
 			</ul>
-
-		<!-- <?php endif; ?> -->
 
 		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
 		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
