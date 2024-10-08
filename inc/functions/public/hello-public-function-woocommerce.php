2ab817a99634506aafa5827992819b7ad475fa86
eliaser-99co <elp@finpr.com>
2024-10-08 14:32:11 +0800

    Custom - Order Receipt

--- inc/functions/public/hello-public-function-woocommerce.php
+++ inc/functions/public/hello-public-function-woocommerce.php
@@ -9,24 +9,27 @@
  */
 
 // Disable add to cart messages
-function setup_single_product_checkout_mode() {
-    if ( get_option( 'hello_theme_checkout_mode' ) === 'single' ) {
+function setup_single_product_checkout_mode()
+{
+    if (get_option('hello_theme_checkout_mode') === 'single') {
         // Disable add to cart messages
-        add_filter( 'wc_add_to_cart_message_html', '__return_false' );
+        add_filter('wc_add_to_cart_message_html', '__return_false');
         // Empty cart before adding a new item
-        add_filter( 'woocommerce_add_cart_item_data', '_hello_theme_additional_empty_cart' );
+        add_filter('woocommerce_add_cart_item_data', '_hello_theme_additional_empty_cart');
     }
 }
-add_action( 'init', 'setup_single_product_checkout_mode' );
+add_action('init', 'setup_single_product_checkout_mode');
 
-function _hello_theme_additional_empty_cart( $cart_item_data ) {
+function _hello_theme_additional_empty_cart($cart_item_data)
+{
     WC()->cart->empty_cart();
     return $cart_item_data;
 }
 
-add_filter( 'woocommerce_add_cart_item_data', 'hello_theme_additional_woo_hello_theme_add_to_cart' );
+add_filter('woocommerce_add_cart_item_data', 'hello_theme_additional_woo_hello_theme_add_to_cart');
 
-function hello_theme_additional_woo_hello_theme_add_to_cart( $cart_item_data ) {
+function hello_theme_additional_woo_hello_theme_add_to_cart($cart_item_data)
+{
 
     global $woocommerce;
     $woocommerce->cart->empty_cart();
@@ -37,24 +40,27 @@ function hello_theme_additional_woo_hello_theme_add_to_cart( $cart_item_data ) {
 
 // Redirect to checkout page after adding an item to the cart
 add_filter('woocommerce_add_to_cart_redirect', 'hello_theme_additional_add_to_cart_redirect');
-function hello_theme_additional_add_to_cart_redirect() {
+function hello_theme_additional_add_to_cart_redirect()
+{
     return wc_get_checkout_url();
 }
 
 // Disable non-base location price adjustments
-add_filter( 'woocommerce_adjust_non_base_location_prices', '__return_false' );
+add_filter('woocommerce_adjust_non_base_location_prices', '__return_false');
 
 // Disable order notes field
 add_filter('woocommerce_enable_order_notes_field', '__return_false');
 
 // Change order button text
-add_filter( 'woocommerce_order_button_text', 'hello_theme_wc_custom_order_button_text' );
-function hello_theme_wc_custom_order_button_text() {
-    return __( 'PROCEED TO PAYMENT', 'woocommerce' );
+add_filter('woocommerce_order_button_text', 'hello_theme_wc_custom_order_button_text');
+function hello_theme_wc_custom_order_button_text()
+{
+    return __('PROCEED TO PAYMENT', 'woocommerce');
 }
 
-add_filter( 'woocommerce_checkout_fields' , 'hello_theme_modify_woocommerce_billing_fields' );
-function hello_theme_modify_woocommerce_billing_fields( $fields ) {
+add_filter('woocommerce_checkout_fields', 'hello_theme_modify_woocommerce_billing_fields');
+function hello_theme_modify_woocommerce_billing_fields($fields)
+{
     // Remove Default Field
     // unset($fields['billing']['billing_company']);    
     // unset($fields['billing']['billing_address_2']);
@@ -68,92 +74,95 @@ function hello_theme_modify_woocommerce_billing_fields( $fields ) {
 }
 
 //add_filter('woocommerce_checkout_fields', 'hello_theme_checkout_fields_order_and_class');
-function hello_theme_checkout_fields_order_and_class($fields) {
+function hello_theme_checkout_fields_order_and_class($fields)
+{
     $billing_fields_order = array(
         'billing_email' => array(
-            'label'       => __('Email', 'woocommerce'),
+            'label' => __('Email', 'woocommerce'),
             'placeholder' => _x('Email address', 'placeholder', 'woocommerce'),
-            'required'    => true,
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => true,
-            'priority' => 10 
+            'required' => true,
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => true,
+            'priority' => 10
         ),
         'billing_first_name' => array(
-            'label'       => __('First Name', 'woocommerce'), 
-            'placeholder' => _x('First name', 'placeholder', 'woocommerce'), 
-            'required'    => true,
-            'class'       => array('form-row-first hello-theme-checkout-field'),
-            'clear'       => false, 
+            'label' => __('First Name', 'woocommerce'),
+            'placeholder' => _x('First name', 'placeholder', 'woocommerce'),
+            'required' => true,
+            'class' => array('form-row-first hello-theme-checkout-field'),
+            'clear' => false,
             'priority' => 20
         ),
         'billing_last_name' => array(
-            'label'       => __('Last Name', 'woocommerce'), 
-            'placeholder' => _x('Last name', 'placeholder', 'woocommerce'), 
-            'required'    => true,
-            'class'       => array('form-row-last hello-theme-checkout-field'),
-            'clear'       => false, 
+            'label' => __('Last Name', 'woocommerce'),
+            'placeholder' => _x('Last name', 'placeholder', 'woocommerce'),
+            'required' => true,
+            'class' => array('form-row-last hello-theme-checkout-field'),
+            'clear' => false,
             'priority' => 30
         ),
-        'billing_address_1' => array( 
-            'label'       => __('Address', 'woocommerce'),
+        'billing_address_1' => array(
+            'label' => __('Address', 'woocommerce'),
             'placeholder' => _x('Street address', 'placeholder', 'woocommerce'),
-            'required'    => false,
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => true,
+            'required' => false,
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => true,
             'priority' => 40
         ),
         'billing_country' => array(
-            'label'       => __('Country', 'woocommerce'), 
-            'required'    => true,
-            'type'        => 'country',
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => true,
+            'label' => __('Country', 'woocommerce'),
+            'required' => true,
+            'type' => 'country',
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => true,
             'priority' => 50
         ),
         'billing_state' => array(
-            'label'       => __('State', 'woocommerce'),
-            'required'    => false,
-            'type'        => 'state',
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => false,
+            'label' => __('State', 'woocommerce'),
+            'required' => false,
+            'type' => 'state',
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => false,
             'priority' => 60
         ),
         'billing_city' => array(
-            'label'       => __('Town / City', 'woocommerce'), // Default label for billing_city
-            'placeholder' => _x('Town / City', 'placeholder', 'woocommerce'), 
-            'required'    => false,
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => true,
+            'label' => __('Town / City', 'woocommerce'), // Default label for billing_city
+            'placeholder' => _x('Town / City', 'placeholder', 'woocommerce'),
+            'required' => false,
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => true,
             'priority' => 70
         ),
         'billing_postcode' => array(
-            'label'       => __('Postcode / ZIP', 'woocommerce'), 
-            'placeholder' => _x('Postcode / ZIP', 'placeholder', 'woocommerce'), 
-            'required'    => false,
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => false, 
+            'label' => __('Postcode / ZIP', 'woocommerce'),
+            'placeholder' => _x('Postcode / ZIP', 'placeholder', 'woocommerce'),
+            'required' => false,
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => false,
             'priority' => 80
         ),
 
         'billing_phone' => array(
-            'label'       => __('Phone', 'woocommerce'),
+            'label' => __('Phone', 'woocommerce'),
             'placeholder' => _x('Phone number', 'placeholder', 'woocommerce'),
-            'required'    => false,
-            'class'       => array('form-row-wide hello-theme-checkout-field'),
-            'clear'       => false, 
+            'required' => false,
+            'class' => array('form-row-wide hello-theme-checkout-field'),
+            'clear' => false,
             'priority' => 90
         ),
     );
     $fields['billing'] = $billing_fields_order;
     return $fields;
 }
-function hello_theme_woocommerce_checkout_terms_and_conditions() {
-  remove_action( 'woocommerce_checkout_terms_and_conditions', 'wc_terms_and_conditions_page_content', 30 );
+function hello_theme_woocommerce_checkout_terms_and_conditions()
+{
+    remove_action('woocommerce_checkout_terms_and_conditions', 'wc_terms_and_conditions_page_content', 30);
 }
-add_action( 'wp', 'hello_theme_woocommerce_checkout_terms_and_conditions' );
+add_action('wp', 'hello_theme_woocommerce_checkout_terms_and_conditions');
 
 // Function to hide specific countries on WooCommerce checkout
-function hello_theme_woocommerce_checkout_hide_countries_on_checkout($countries) {
+function hello_theme_woocommerce_checkout_hide_countries_on_checkout($countries)
+{
     // Array of country codes to hide
     $countries_to_hide = array(''); // Add the country codes you want to hide
 
@@ -168,5 +177,15 @@ function hello_theme_woocommerce_checkout_hide_countries_on_checkout($countries)
 
 // Hook the function to the WooCommerce checkout fields filter
 add_filter('woocommerce_countries', 'hello_theme_woocommerce_checkout_hide_countries_on_checkout', 10, 1);
+
+
+// Custom - Order Received
+function check_order_received_page()
+{
+    if (is_wc_endpoint_url('order-received')) {
+        require_once get_stylesheet_directory() . '/inc/functions/custom/order-received.php';
+    }
+}
+add_action('wp', 'check_order_received_page');
 
 ?>
